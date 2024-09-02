39d2c1128be7cc98391de792c1ebe8c2114bd5be
Anthony <1249632746@qq.com>
2024-09-02 22:01:33 +0800

    队列实现栈

---
+++ stack/225. 用队列实现栈.js
@@ -0,0 +1,40 @@
+
+var MyStack = function() {
+    this.queue = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+    this.queue.push(x);
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+    let size = this.queue.length
+    while(size-- > 1){
+        this.queue.push(this.queue.shift());
+    }
+    return this.queue.shift()
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+    const x = this.pop();
+    this.queue.push(x);
+    return x;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+    return !this.queue.length;
+};
+
