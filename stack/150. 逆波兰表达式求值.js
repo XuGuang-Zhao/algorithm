d4830c304b4d6b6e3dc04c7382b9f2d5548d2192
Anthony <1249632746@qq.com>
2024-09-03 22:29:49 +0800

    逆波兰

---
+++ stack/150. 逆波兰表达式求值.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+    const stack = []
+    const operation = ['+', '-', '*', '/']
+    tokens.forEach(token => {
+        if (operation.includes(token)) {
+            const num2 = stack.pop()
+            const num1 = stack.pop()
+            if (token === '+') stack.push(num1 + num2)
+            if (token === '-') stack.push(num1 - num2)
+            if (token === '*') stack.push(num1 * num2)
+            if (token === '/') stack.push(Math.trunc(num1 / num2))
+        } else {
+            stack.push(Number(token))
+        }
+    })
+    return stack.shift()
+};
